3a4e77bfa83880a2a2add7ebb6ccdceed29c0592
Chris <oscar.sletvold@gmail.com>
2024-03-05 10:28:37 +0100

    update

---
+++ script.js
@@ -0,0 +1,50 @@
+//M
+let selectedLampIndex = -1;
+let startTime;
+//V
+updateView();
+function updateView() {
+    let html = '';
+    for(i = 1; i < 26; i++) {
+        html += `<div class="lamp" id="lamp${i}" onclick="selectedLamp(${i})">${i}</div>`
+    }
+    document.getElementById('app').innerHTML = /*HTML*/ `
+        <h3>Reaksjons Spill<h3>
+        <div class="container">
+            ${html}
+        </div>
+        <div id="timeResults"></div>
+    `;
+    if(selectedLampIndex !== -1) {
+        turnLightOn(selectedLampIndex);     
+    }
+}
+
+//C
+document.addEventListener('DOMContentLoaded', () => {
+    randomLightOn();
+});
+
+function randomLightOn() {
+    const newSelectedIndex = Math.floor(Math.random() * 25) + 1;
+    turnLightOn(newSelectedIndex);
+    startTime = new Date().getTime();
+}
+
+function turnLightOn(i) {
+    if(selectedLampIndex !== -1) {
+        document.getElementById(`lamp${selectedLampIndex}`).classList.remove('lightOn');
+    }
+    document.getElementById(`lamp${i}`).classList.add('lightOn');
+    selectedLampIndex = i;
+}
+
+function selectedLamp(i) {
+    if (i === selectedLampIndex) { // Only proceed if the clicked lamp is the currently highlighted one
+        let finishTime = new Date().getTime();
+        let spentMilliseconds = finishTime - startTime;
+        let spentSeconds = spentMilliseconds / 1000;
+        document.getElementById('timeResults').innerHTML += `Reaction Time: ${spentSeconds} seconds.<br>`;
+        randomLightOn(); // Highlight another lamp
+    }
+}
